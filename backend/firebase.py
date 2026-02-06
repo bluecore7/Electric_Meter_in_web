a35c8fd121873221510f60a2f3ea1574eca6fc63
bluecore <bluecorexux@gmail.com>
2026-02-07 02:34:21 +0530

    update 0.2.2

--- backend/firebase.py
+++ backend/firebase.py
@@ -5,20 +5,27 @@ import base64
 import firebase_admin
 from firebase_admin import credentials, db
 
-key_base64 = os.environ.get("FIREBASE_KEY_BASE64")
-if not key_base64:
-    raise RuntimeError("FIREBASE_KEY_BASE64 not set")
+def init_firebase():
+    if firebase_admin._apps:
+        return
 
-cred_dict = json.loads(base64.b64decode(key_base64).decode("utf-8"))
+    key_base64 = os.environ.get("FIREBASE_KEY_BASE64")
+    if not key_base64:
+        raise RuntimeError("FIREBASE_KEY_BASE64 not set")
 
-cred = credentials.Certificate(cred_dict)
+    cred_dict = json.loads(
+        base64.b64decode(key_base64).decode("utf-8")
+    )
 
-firebase_admin.initialize_app(
-    cred,
-    {
-        "databaseURL": "https://energyflow-esp32-default-rtdb.firebaseio.com"
-    }
-)
+    cred = credentials.Certificate(cred_dict)
+
+    firebase_admin.initialize_app(
+        cred,
+        {
+            "databaseURL": "https://energyflow-esp32-default-rtdb.firebaseio.com"
+        }
+    )
 
 def get_db():
+    init_firebase()
     return db.reference("/")
